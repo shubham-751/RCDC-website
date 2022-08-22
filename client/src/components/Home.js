4b77930796410c4ca8c664020707244ef0e6a863
atharvawadekar <atharvawadekar2908@gmail.com>
2022-08-22 22:48:54 +0530

    dropdown attempt

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className="home">
+            <h1>Home</h1>
+        </div>
     );
 }
  
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 const Navbar = () => {
     return (
         <div className="navbar">
-            <div className="navelement">Rotract</div>
-            <Link className="navelement" to="/">Home</Link>
-            <Link className="navelement" to="/projects">Projects</Link>
-            <Link className="navelement" to="/contactus">Contact Us</Link>
+            <div className="navelement title">Rotract</div>
+            <div className="navelement logo">Logo</div>
+            <div className="navelement">
+                <DropdownButton id="dropdown-basic-button" title="Information">
+                        <div className="dropdownelement"><Link className="btn" to="/">Home</Link></div>
+                        <div className="dropdownelement"><Link className="btn" to="/projects">Projects</Link></div>
+                        <div className="dropdownelement"><Link className="btn" to="/contactus">Contact Us</Link></div>
+                </DropdownButton>
+            </div>
         </div>
     );
 }
