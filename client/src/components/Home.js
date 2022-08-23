06171401f3206f149484e1c43864c4e61acb6f96
Shubham Pal <88453955+shubham-751@users.noreply.github.com>
2022-08-23 17:19:36 +0530

    logo

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Img from  './rcdc.png';
 
 const Home = () => {
     return (
         <div className="home">
-            <h1>Home</h1>
+		
+        <center> 
+        <h1>HOME</h1>
+        <img src= {Img} alt="pic" />
+        <h1>ABOUT US</h1>
+        </center>
+        
         </div>
     );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import Image from './logo.png';
 
 const Navbar = () => {
     return (
         <div className="navbar">
             <div className="navelement title">Rotract</div>
-            <div className="navelement logo">Logo</div>
+            <center>
+            <img src= {Image} alt="pic" /> 
+            </center>
+           
             <div className="navelement">
                 <DropdownButton id="dropdown-basic-button" title="Information">
                         <div className="dropdownelement"><Link className="btn" to="/">Home</Link></div>
                         <div className="dropdownelement"><Link className="btn" to="/projects">Projects</Link></div>
                         <div className="dropdownelement"><Link className="btn" to="/contactus">Contact Us</Link></div>
                 </DropdownButton>
+
             </div>
         </div>
     );
