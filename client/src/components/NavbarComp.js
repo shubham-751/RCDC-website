c0b8dacf9e88d1844d27860ad9326045bc858087
atharvawadekar <atharvawadekar2908@gmail.com>
2022-08-24 23:02:06 +0530

    nav-toggler remaining

--- client/src/components/NavbarComp.js
+++ client/src/components/NavbarComp.js
@@ -3,20 +3,13 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
-import Img from '../images/logo.png';
+import RotractLogo from '../images/logo.png';
 const NavbarComp = () => {
     return (
-        // <div classNameName="navbar">
-        //     <div classNameName="navelement title">Rotract</div>
-        //     <div classNameName="navelement"><Link classNameName="btn" to="/">Home</Link></div>
-        //     <div classNameName="navelement"><Link classNameName="btn" to="/projects">Projects</Link></div>
-        //     <div classNameName="navelement"><Link classNameName="btn" to="/contactus">Contact Us</Link></div>
-        // </div>
         <Navbar className="border-bottom border-white" bg="dark" expand="lg">
             <Container>
-                {/* <img src={Img}  alt='pic'></img> */}
-                <Navbar.Brand><img className="logo" src={Img}  alt='pic'></img></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Brand><img className="logo" src={RotractLogo}  alt='Logo'/></Navbar.Brand>
+                <Navbar.Toggle className="text-light rounded-3" aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
                     <Link to="/" className="nav-link text-light">Home</Link>
