6fe859b7db9b7cc32ce25b711a81e093294d86cf
atharvawadekar <atharvawadekar2908@gmail.com>
2022-08-24 20:26:40 +0530

    img folder placement

--- client/src/components/NavbarComp.js
+++ client/src/components/NavbarComp.js
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
-import Img from './images/logo.png';
+import Img from '../images/logo.png';
 const NavbarComp = () => {
     return (
         // <div classNameName="navbar">
