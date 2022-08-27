445172c570ea5adf98cdfee10c4476900f747fb3
Shubham Pal <88453955+shubham-751@users.noreply.github.com>
2022-08-27 13:49:18 +0530

    homepage

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import NavbarComp from './components/NavbarComp';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Contactus from './components/Contactus';
+import Team from './components/Team';
+
 
 function App() {
   return (
@@ -14,10 +16,11 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/team" element={<Team />} />
           <Route path="/contactus" element={<Contactus />} />
         </Routes>
       </Router>
-    </div>
+      </div>
   );
 }
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,17 +1,46 @@
 import React from "react";
 import Img from  '../images/rcdc.png';
+import {Row,Card} from "react-bootstrap";
 
 const Home = () => {
     return (
+       
         <div className="home">
 		
         <center> 
         <h1>HOME</h1>
-        <img src= {Img} alt="pic" />
-        <h1>ABOUT US</h1>
         </center>
+        <Row>
+        <center>
+        <img src= {Img} class="img-fluid,rounded" alt="pic" />
+        </center>
+        </Row>
+        
+        <Row>
+      <Card class="text-center my-5 py-4 bg-secondary text-white " >
+        <Card.Body>
+        <h1>About US</h1>
+        </Card.Body>
+      </Card>
+      </Row>
+      <p>
+        <center>
+It's not only writers who can benefit from this free online tool. If you're a programmer who's working on a project where blocks of text are needed,
+this tool can be a great way to get that. It's a good way to test your programming and that the tool being created is working well.
+Above are a few examples of how the random paragraph generator can be beneficial. The best way to see if this random paragraph picker
+will be useful for your intended purposes is to give it a try. Generate a number of paragraphs to see if they are beneficial to your current project.
+        </center>
+        </p>
+        <footer class="py-5 my-5 bg-black">
+        <container className="px-4">
+            <p class="text-center text-white">
+Copyright &copy; RCDC Website 2022
+            </p>
+        </container>
+        </footer>
         
         </div>
+        
     );
 }
 
