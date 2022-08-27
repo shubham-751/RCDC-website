3fad77f72253ef85567d68e3be29c726c5cbf4f5
Shubham Pal <88453955+shubham-751@users.noreply.github.com>
2022-08-27 17:39:29 +0530

    ...

--- client/src/components/Contactus.js
+++ client/src/components/Contactus.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 const Contactus = () => {
     return (
+       <Container>
         <h1>Contactus</h1>
+        <footer class="py-4 my-7 bg-black">
+        <container className="px-4">
+            <p class="text-center text-white">
+Copyright &copy; RCDC Website 2022
+            </p>
+        </container>
+        </footer>
+       </Container> 
     );
 }
  
--- client/src/components/Team.js
+++ client/src/components/Team.js
@@ -1,4 +1,7 @@
 import React, { useEffect } from "react";
+import { Container } from "react-bootstrap";
+import Image from '../images/1.jpg';
+import Img from '../images/2.jpg';
 
 const Projects = () => {
 
@@ -7,14 +10,45 @@ const Projects = () => {
     }, []);
 
     return (
-       <div>
-        <div class="spinner-grow" role="status">
-  <span class="visually-hidden">Loading...</span>
-</div>
-<h1>Team</h1>
-       </div>
-        
-        
+        /*<div class="card" style={{
+        width:'25rem',
+          }} >
+  <img src={Image} class="card-img-top " alt="2" />
+  <div class="card-body">
+    <p class="card-text">
+        DIGITAL COMMUNICATION DIRECTOR
+        <h1> Shubham Shyamdas pal</h1>
+    </p>
+  </div>
+</div>*/
+<Container class="container text-center">
+  <div class="row row-cols-2">
+    <div class="col"><div class="card" style={{
+        width:'25rem',
+          }} >
+  <img src={Image} class="card-img-top " alt="1" />
+  <div class="card-body">
+    <p class="card-text">
+        DIGITAL COMMUNICATION DIRECTOR
+        <h1> Shubham Shyamdas pal</h1>
+    </p>
+  </div>
+</div></div>
+    <div class="col"><div class="card" style={{
+        width:'25rem',
+          }} >
+  <img src={Img} class="card-img-top " alt="2" />
+  <div class="card-body">
+    <p class="card-text">
+        DIGITAL COMMUNICATION DIRECTOR
+        <h1> Parth Phansalkar</h1>
+    </p>
+  </div>
+</div></div>
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+  </div>
+</Container>
     );
 }
  
